593bea9ce63facc324ee701d328fbf29875cedc6
Кира <gruppa1@gmail.com>
2025-07-31 21:43:41 +0300

    Добавляем функции

--- Cryptocurrency.py
+++ Cryptocurrency.py
@@ -3,16 +3,48 @@ from tkinter import ttk
 from tkinter import messagebox as mb
 import requests
 
+
 def update_b_label(event):
     code = base_combobox.get()
     name = currencies_b[code]
     b_label.config(text=name)
+
 
 def update_t_label(event):
     # Получаем полное название целевой валюты из словаря и обновляем метку
     code = target_combobox.get()
     name = currencies_names[code]
     t_label.config(text=name)
+
+
+
+def exchange():
+    target_code = target_combobox.get()
+    base_code = base_combobox.get()
+
+    if target_code and base_code:
+        try:
+            coin_id = currencies[target_code]
+            vs = base_code.lower()
+            url =  f"https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies={vs}"
+            response = requests.get(url)
+            response.raise_for_status()
+
+            data = response.json()
+
+            if coin_id in data and vs in data[coin_id]:
+                price = data[coin_id][vs]
+                base = currencies_b[base_code]
+                target_name = currencies_names[target_code]
+                mb.showinfo("Курс обмена", f" 1 {target_name} ={price} {base_code} ({base})")
+            else:
+                mb.showerror("Ошибка", f"Валюта {target_code} не найдена")
+        except Exception as e:
+            mb.showerror("Ошибка", f"Ошибка: {e}")
+    else:
+        mb.showwarning("Внимание", "Выберите коды валют")
+
+
 
 currencies_b ={
     'USD':"Американский доллар",
