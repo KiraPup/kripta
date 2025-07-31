f5c26e55a16377b505682376e00f82f3dff06be2
Кира <gruppa1@gmail.com>
2025-07-31 16:47:15 +0300

    Создали окно

--- Крипта.py
+++ Крипта.py
@@ -3,3 +3,13 @@ import json
 import pprint
 from tkinter import *
 from tkinter import messagebox as mb
+
+
+window = Tk()
+window.title("Чайник в крипте -  Курсы криптовалют")
+window.geometry('500x400')
+
+
+
+
+window.mainloop()
