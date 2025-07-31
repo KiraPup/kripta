f61b5ed9161cf93b4fe69af7d4ecedde97479c9e
Кира <gruppa1@gmail.com>
2025-07-31 19:22:17 +0300

    создание интерфейса

--- Криптовалюта.py
+++ Криптовалюта.py
@@ -2,3 +2,28 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox as mb
 import requests
+
+
+window = Tk()
+window.title("Актуальная информация о криптовалюте")
+window.geometry("360x300")
+
+Label(text="Базовая валюта:").pack(padx=10, pady=5)
+base_combobox = ttk.Combobox(values=list(currencies.keys()))
+base_combobox.pack(padx=10, pady=5)
+base_combobox.bind("<<ComboboxSelected>>", update_b_label)
+
+b_label = ttk.Label()
+b_label.pack(padx=10, pady=10)
+
+Label(text="Криптовалюта:").pack(padx=10, pady=5)
+target_combobox = ttk.Combobox(values=list(currencies.keys()))
+target_combobox.pack(padx=10, pady=5)
+target_combobox.bind("<<ComboboxSelected>>", update_t_label)
+
+t_label = ttk.Label()
+t_label.pack(padx=10, pady=10)
+
+Button(text="Получить цену на криптовалюту", command=exchange).pack(padx=10, pady=10)
+
+window.mainloop()
